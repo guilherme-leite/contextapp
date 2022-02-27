96cadf13b6de255ea68855317417fee82ff85f5a
Guilherme Leite <guilhermelax@hotmail.com>
2022-02-27 13:36:39 -0300

    Feat: Criando componente BookList e adicionando na App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BookList from './components/BookList';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <BookList />
     </div>
   );
 }
