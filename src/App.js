c6c524cef7683d2a18139e0e6fb3d125d318eaa8
Guilherme Leite <guilhermelax@hotmail.com>
2022-02-28 13:09:03 -0300

    Fix: Alterando Component NavBar e corrigindo erro.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import BookList from './components/BookList';
 import NavBar from './components/NavBar';
 import ThemeToggle from './components/ThemeToggle';
 import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <NavBar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <NavBar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
