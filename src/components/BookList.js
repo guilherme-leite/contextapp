1ce51fd5b9799b4e3749d51f33894cc0406d3fdf
Guilherme Leite <guilhermelax@hotmail.com>
2022-02-28 16:06:22 -0300

    Feat: Transformando BookList.js em sfc

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
+// import React, { Component, useContext } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-    render() { 
-      const { isLightTheme, light, dark } = this.context;
-      const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>The way of kings</li>
-          <li style={{ background: theme.ui }}>The name of the wind</li>
-          <li style={{ background: theme.ui }}>Anthem</li>
-        </ul>
-      </div>
-    );
-  }
-}
+// Class component usando contextType.
 
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+//     render() { 
+//       const { isLightTheme, light, dark } = this.context;
+//       const theme = isLightTheme ? light : dark;
+//     return (
+//       <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>The way of kings</li>
+//           <li style={{ background: theme.ui }}>The name of the wind</li>
+//           <li style={{ background: theme.ui }}>Anthem</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default BookList;
+
+// Stateless function component utilizando useContexte
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }}>The way of kings</li>
+        <li style={{ background: theme.ui }}>The name of the wind</li>
+        <li style={{ background: theme.ui }}>Anthem</li>
+      </ul>
+    </div>
+  );
+}
+ 
 export default BookList;
